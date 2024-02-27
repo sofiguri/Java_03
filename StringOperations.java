e2d2b09cf58f66348a0d3f7f9367cc7f06d91446
Гурьянова Софья <130737896+sofiguri@users.noreply.github.com>
2024-02-27 18:35:26 +0300

    Add files via upload

--- StringOperations.java
+++ StringOperations.java
@@ -2,40 +2,38 @@ import java.util.Scanner;
 
 public class StringOperations {
     public static void inputStrings(Scanner scanner) {
-        System.out.println("Введите первую строку: ");
+        System.out.println("Введите несколько чисел через пробел:");
         // scanner.next();
-        String str1 = scanner.nextLine();
-        System.out.println("Введите вторую строку: ");
-        String str2 = scanner.nextLine();
-
-        // Вывод строк в терминал
-        System.out.println("Введены строки:\n(1) " + str1 + "\n(2) " + str2);
+        String  input = scanner.nextLine();
+        String[] numbers = input.split(" ");
         // Сохранение в бд
-        DBhandler.saveString(str1, str2);
+        DBhandler.saveString(input);
+        System.out.println("Введенные числа:");
+        for (String num : numbers) {
+            System.out.println(num);
+        }
 
     }
 
-    public static void StringSize() {
+    public static void StringIntegrity() {
+        try {
+            int number = scanner.nextInt();
 
-        String str1 = DBhandler.getStrFromDB(1);
-        String str2 = DBhandler.getStrFromDB( 2);
-        int len1 = str1.length();
-        int len2 = str2.length();
-        System.out.println("[" + str1 + "]" + " Длинна = " + len1);
-        System.out.println("[" + str2 + "]" + " Длинна = " + len2);
-        DBhandler.insertStrSize(len1, len2);
-    }
+            if (number % 1 == 0) {
+                System.out.println("Целое");
+            } else {
+                System.out.println("Не целое");
+            }
+        }
 
-    public static void StringConcat(){
-        String str1 = DBhandler.getStrFromDB(1);
-        String str2 = DBhandler.getStrFromDB(2);
-        String str3 = str1.concat(str2);
-        DBhandler.insertStrConcat(str3);
-    }
-    public static void StringCompare(){
-        String str1 = DBhandler.getStrFromDB(1);
-        String str2 = DBhandler.getStrFromDB(2);
-        int compare = str1.compareTo(str2);
-        DBhandler.insertComparison(compare);
-    }
+    public static void StringParity(){
+            try {
+                int number = scanner.nextInt();
+
+                if (number % 2 == 0) {
+                    System.out.println("Четное");
+                } else {
+                    System.out.println("Нечетное");
+                }
+            }
 }
